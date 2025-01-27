4c3255eaf9000e737ce5a6f39c6e4e6dbc2216c3
Rejeq <jere3690@gmail.com>
2025-01-27 16:20:17 +0300

    add: Publish to maven central

--- buildSrc/src/main/kotlin/ktobs.publish.gradle.kts
+++ buildSrc/src/main/kotlin/ktobs.publish.gradle.kts
@@ -1,9 +1,40 @@
-group = "com.rejeq.ktobs"
-version = "5.5.4"
+import com.vanniktech.maven.publish.SonatypeHost
+
+group = "io.github.rejeq"
+version = "0.1.0"
 
 plugins {
     id("com.vanniktech.maven.publish")
 }
 
 mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    pom {
+        inceptionYear = "2025"
+        url = "https://github.com/rejeq/ktobs/"
+
+        licenses {
+            license {
+                name = "MIT License"
+                url = "http://www.opensource.org/licenses/mit-license.php"
+            }
+        }
+
+        developers {
+            developer {
+                id = "rejeq"
+                name = "rejeq"
+                url = "https://github.com/rejeq/"
+            }
+        }
+
+        scm {
+            url = "https://github.com/rejeq/ktobs/"
+            connection = "scm:git:git://github.com/rejeq/ktobs.git"
+            developerConnection = "scm:git:ssh://git@github.com/rejeq/ktobs.git"
+        }
+    }
 }
